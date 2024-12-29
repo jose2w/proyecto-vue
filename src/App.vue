9c445d1ddf8502b529cd912291e74fd062bc662d
Jose <jose2001xdh@gmail.com>
2024-12-28 20:37:44 -0500

    Crea componente Lista De Traeas Borra componentes autogenerados Crea rutas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 
 <template>
-<Contador></Contador>
+  <MainView></MainView>
+  <Contador></Contador>
+  <ListaDeTareas></ListaDeTareas>
 </template>
 
 <script setup>
-import Contador from './components/Contador.vue';
+  import MainView from './views/MainView.vue';
+  
 </script>
 
 <style scoped>
